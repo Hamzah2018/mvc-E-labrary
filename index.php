3fc8e8663d2dcaeaef73c974ce3f0df9ee9c49f6
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-03-26 23:33:56 +0300

    m

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-require_once 'app/system/app.php';
-$app=new App();?>
+//require_once 'app/system/app.php';
+//$app=new App();
+// use 
+//require_once 'app/system/app.php';
+require_once __DIR__ .  'vendor/autoload.php';
+
+$app = new AppSystem();
+// use SecTheater\Http\Route;
+
+// require_once __DIR__ . './vendor/autoload.php';
+// // require_once __DIR__  . './routes/web.php';
+//  var_dump(Route::$routes['get']['/']->call());
+?>
