ee6b20a6d34e94814c64028c33d4be7b9d21bb37
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-03-29 17:25:49 +0300

    some changes

--- public/index.php
+++ public/index.php
@@ -1,18 +1,46 @@
 <?php
-//require_once 'app/system/app.php';
-//$app=new App();
-// use 
-//require_once 'app/system/app.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-use MvcELabrary\app\system\AppSystem;
+use eliber\app\controllers\AuthorsController;
+use eliber\app\controllers\PublishersController;
+use eliber\app\system\AppSystem;
+use eliber\app\system\Router;
+use eliber\app\controllers\UsersController;
+use Dotenv\Dotenv;
 
-require_once __DIR__ .  '/vendor/autoload.php';
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));//createImmutable(__DIR__);
+$dotenv->load();
+
+$config=array(
+  'servername'=>$_ENV['DB_SERVER_NAME'],
+  'dbname'=>$_ENV['DB_NAME'],
+  'dbpass'=>$_ENV['DB_PASSWORD'],
+  'username'=>$_ENV['DB_USERNAME']
+
+);
+$system=new AppSystem($config);
 
 
-$app = new AppSystem();
-// use SecTheater\Http\Route;
+Router::get('/users',[UsersController::class,'show']);
 
-// require_once __DIR__ . './vendor/autoload.php';
-// // require_once __DIR__  . './routes/web.php';
-//  var_dump(Route::$routes['get']['/']->call());
-?>
+Router:: get ('/', function () {
+  return 'Welcome to index';
+});
+
+
+
+Router::get('/books',function(){
+  echo "books route path";
+});
+
+Router::get('/new_user',[UsersController::class,'register']);
+
+Router::get('/remove_user',[UsersController::class,'delete']);
+
+Router::post('/users',[UsersController::class,'show']);
+Router::get('/new_user',[UsersController::class,'newUser']);
+
+Router::post('/save_user',[UsersController::class,'saveUser']);
+Router::get('/save_author',[AuthorsController::class,'createAuthor']);
+$system->start();
+
